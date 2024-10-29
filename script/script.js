76a1f09bfb34bc5bd8568e3a494c9fa7111da20f
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-10-29 14:13:39 +0100

    check numbers divisible by 3

--- script/script.js
+++ script/script.js
@@ -13,6 +13,8 @@ for (let i = 1; i <= 100; i++) {
 // verifico quali numeri sono divisibili per 3 e per 5
 if (i % 3 === 0 && i % 5 === 0) {
     console.log('FizzBuzz');
-}
+} else if (i % 3 === 0) { // verifico quali nuemri sono divisibili solo per 3
+    console.log('Fizz');
+} 
     console.log(i);
 }
