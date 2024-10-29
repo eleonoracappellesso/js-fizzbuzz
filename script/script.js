89267b796fe604e15bb25672119b80af758455f9
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-10-29 14:33:46 +0100

    check numbers divisible by 5

--- script/script.js
+++ script/script.js
@@ -10,11 +10,14 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 // creo un ciclo per stampare i numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-// verifico quali numeri sono divisibili per 3 e per 5
-if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz');
-} else if (i % 3 === 0) { // verifico quali nuemri sono divisibili solo per 3
-    console.log('Fizz');
-} 
-    console.log(i);
+    // verifico se i è divisibile per 3
+    if (i % 3 === 0) {
+        if (i % 5 === 0) { // verifico se è divisibile anche per 5
+            console.log('FizzBuzz');
+        } else {
+            console.log('Fizz'); // output se divisibile solo per 3 
+        }
+    } else if (i % 5 === 0) { // verifico se è divisibile per 5
+        console.log('Buzz');
+    } 
 }
